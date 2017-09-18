691f752b70ff741db67d916146722dd83e7e294a
Jacob Lucas <jacob.lucas@grab.com>
2017-09-18 19:01:24 +0800

    day 3 part 2

--- src/main/scala/com/jacoblucas/adventofcode2015/Day03.scala
+++ src/main/scala/com/jacoblucas/adventofcode2015/Day03.scala
@@ -6,32 +6,40 @@ import scala.io.Source
 object Day03 {
 
   // x, y coordinates -> count
-  val presents: Map[(Int, Int), Int] = Map(((0,0), 1))
+  val presents: Map[(Int, Int), Int] = Map(((0,0), 2))
+
+  def deliver(loc: (Int, Int), presents: Map[(Int, Int), Int]): Map[(Int, Int), Int] =
+    presents.get(loc) match {
+      case Some(n) => presents.updated(loc, n + 1)
+      case None => presents + (loc -> 1)
+    }
+
+  def getNewLocation(currentLoc: (Int, Int), x: Char): (Int, Int) =
+    x match {
+      case '>' => (currentLoc._1 + 1, currentLoc._2) // x + 1
+      case '<' => (currentLoc._1 - 1, currentLoc._2) // x - 1
+      case '^' => (currentLoc._1, currentLoc._2 + 1) // y + 1
+      case 'v' => (currentLoc._1, currentLoc._2 - 1) // y - 1
+    }
 
   @tailrec
-  def deliver(instructions: List[Char], presents: Map[(Int, Int), Int], currentLoc: (Int, Int)): Map[(Int, Int), Int] = {
+  def deliver(instructions: List[Char], presents: Map[(Int, Int), Int], currentSantaLoc: (Int, Int), currentRoboSantaLoc: (Int, Int), n: Int): Map[(Int, Int), Int] = {
     instructions match {
       case x :: xs =>
-        val newLoc = x match {
-          case '>' => (currentLoc._1 + 1, currentLoc._2) // x + 1
-          case '<' => (currentLoc._1 - 1, currentLoc._2) // x - 1
-          case '^' => (currentLoc._1, currentLoc._2 + 1) // y + 1
-          case 'v' => (currentLoc._1, currentLoc._2 - 1) // y - 1
+        if (n % 2 == 0) {
+          val newLoc = getNewLocation(currentRoboSantaLoc, x)
+          deliver(xs, deliver(newLoc, presents), currentSantaLoc, newLoc, n + 1)
+        } else {
+          val newLoc = getNewLocation(currentSantaLoc, x)
+          deliver(xs, deliver(newLoc, presents), newLoc, currentRoboSantaLoc, n + 1)
         }
-
-        val updated = presents.get(newLoc) match {
-          case Some(n) => presents.updated(newLoc, n + 1)
-          case None => presents + (newLoc -> 1)
-        }
-
-        deliver(xs, updated, newLoc)
       case Nil => presents
     }
   }
 
   def main(args: Array[String]): Unit = {
     val lines = Source.fromInputStream(getClass.getResourceAsStream("/day03-input.txt")).mkString
-    val result = deliver(lines.toList, presents, (0,0))
+    val result = deliver(lines.toList, presents, (0,0), (0,0), 1)
     println(result.size)
   }
 
