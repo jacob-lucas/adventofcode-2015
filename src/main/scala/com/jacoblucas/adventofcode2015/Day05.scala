fe250c54bf704b4fefab6b1c32914a21b0e87138
Jacob Lucas <jacob.lucas@grab.com>
2017-09-21 03:37:06 +0800

    day 5 part 2

--- src/main/scala/com/jacoblucas/adventofcode2015/Day05.scala
+++ src/main/scala/com/jacoblucas/adventofcode2015/Day05.scala
@@ -5,20 +5,17 @@ import scala.io.Source
 
 object Day05 {
 
-  def containsThreeVowels(str: String): Boolean = {
+  def containsThreeVowels(str: String): Boolean =
     List[Char]('a', 'e', 'i', 'o', 'u')
       .map(v => str.count(_ == v))
       .sum >= 3
-  }
 
   @tailrec
-  def containsRepeatingLetter(str: String): Boolean = {
+  def containsRepeatingLetter(str: String): Boolean =
     str.toList match {
       case x :: y :: xs => if (x == y) true else containsRepeatingLetter((List(y) ++ xs).mkString)
-      case x :: Nil => false
-      case Nil => false
+      case _ => false
     }
-  }
 
   def containsSubstring(str: String): Boolean =
     List[String]("ab", "cd", "pq", "xy").exists(str.contains(_))
@@ -26,16 +23,15 @@ object Day05 {
   def isNice(str: String): Boolean =
     containsThreeVowels(str) && containsRepeatingLetter(str) && !containsSubstring(str)
 
+  def matcher(pattern: String): String => Boolean = pattern.r.unanchored.findFirstIn(_).isDefined
+  def repeatWithOneLetterBetween: String => Boolean = matcher("""(\w)\w\1""")
+  def pairAppearsTwice: String => Boolean = matcher("""(\w)(\w).*\1\2""")
+  def isNiceV2(str: String): Boolean = pairAppearsTwice(str) && repeatWithOneLetterBetween(str)
+
   def main(args: Array[String]): Unit = {
     val lines = Source.fromInputStream(getClass.getResourceAsStream("/day05-input.txt")).mkString.split("\n")
-
-//    println(isNice("ugknbfddgicrmopn"))
-//    println(isNice("aaa"))
-//    println(!isNice("jchzalrnumimnmhp"))
-//    println(!isNice("haegwjzuvuyypxyu"))
-//    println(!isNice("dvszwmarrgswjxmb"))
-
     println(lines count isNice)
+    println(lines count isNiceV2)
   }
 
 }
